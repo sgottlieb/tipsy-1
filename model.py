54021ae4d2da0cf9f8416ca5f053c9eb755aa545
Student <student@Administrators-Mac-mini.local>
2012-10-31 15:12:19 -0700

    final version

--- model.py
+++ model.py
@@ -1,93 +1,212 @@
 """
 model.py
 """
-import sqlite3
+import sqlite3 
+import datetime
+
 
 def connect_db():
     return sqlite3.connect("tipsy.db")
 
-def new_user(db, email, password, name):          
-    c = db.cursor()                                     
-    query = """INSERT INTO Users VALUES (NULL, ?, ?, ?)"""                                                           
-    res = c.execute(query, (email, password, name))           
-    if res:
-        db.commit()
-        return res.lastrowid
-
-def make_user(row):
-    fields = ["id", "email", "password", "username"]
-    return dict(zip(fields, row))
-
-def authenticate(db, email, password):
+def get_from_table_by_id(db, table_name, id, make_dict_fn):
     c = db.cursor()
-    query = """SELECT * from Users WHERE email=? AND password=?"""
-    c.execute(query, (email, password))
-    result = c.fetchone()
-    if result:
-        fields = ["id", "email", "password", "username"]
-        return make_user(result)
-
+    query_template = """SELECT * from %s WHERE id = ?"""
+    query = query_template%table_name
+    c.execute(query, (id,))
+    row = c.fetchone()
+    if row:
+        return make_dict_fn(row)
     return None
 
-def get_user(db, user_id):
-    """Gets a user dictionary out of the database given an id"""
-    c = db.cursor()
-    query = """SELECT * from Users WHERE id = ?"""
-    c.execute(query, (user_id,))
-    user_row = c.fetchone()
-    if user_row:
-        return make_user(user_row)
+class User(object):
+    COLS = ['id', 'email', 'password', 'username']
+    TABLE_NAME = "Users"
+    def __init__(self, id, email, password, name):
+        self.id = id
+        self.email = email
+        self.password = password
+        self.name = name
 
-    return None
+    @classmethod
+    def new(cls, db, email, password, name): 
+        vals = [email, password, name]
+        return insert_into_db(db, cls.TABLE_NAME, cls.COLS, vals)
 
-def new_task(db, title, user_id = None):
-    """Given a title and a user_id, create a new task belonging to that user. Return the id of the created task"""
-    c = db.cursor()
-    query = """INSERT into Tasks values (null, ?, DATETIME('now'), null, ?)"""
-    res = c.execute(query, (title, user_id))
-    if res:
-        db.commit()
-        return res.lastrowid
+    @classmethod
+    def make(row):
+        return User(row[0], row[1], row[2], row[3])
 
-def complete_task(db, task_id):
-    """Mark the task with the given task_id as being complete."""
-    c = db.cursor()
-    query = """UPDATE Tasks SET completed_at=DATETIME('now') WHERE id=?"""
-    res = c.execute(query, (task_id,))
-    if res:
-        db.commit()
-        return res.lastrowid
-    else:
+    @classmethod   
+    def authenticate(cls, db, email, password):
+        c = db.cursor()
+        query = """SELECT * from %s WHERE email=? AND password=?"""%(cls.TABLE_NAME)
+        c.execute(query, (email, password))
+        result = c.fetchone()
+        if result:
+            return cls(*result)
         return None
 
-def get_tasks(db, user_id=None):
-    """Get all the tasks matching the user_id, getting all the tasks in the system if the user_id is not provided. Returns the results as a list of dictionaries."""
+    @classmethod
+    def get_user(cls, db, user_id):
+        return get_from_table_by_id(db, cls.TABLE_NAME, user.id, User.make)
+
+    @classmethod
+    def get_tasks(cls, db, user_id=None):
+        """Get all the tasks matching the user_id, getting all the tasks in the system if the user_id is not provided. Returns the results as a list of dictionaries."""
+        c = db.cursor()
+        if user_id:
+            query = """SELECT * from Tasks WHERE user_id = ?"""
+            c.execute(query, (user_id, ))
+        else:
+            query = """SELECT * from Tasks"""
+            c.execute(query)
+        tasks = []
+        rows = c.fetchall()
+        for row in rows:
+            task = Task.make(row)
+            tasks.append(task)
+
+        return tasks
+
+
+class Task(object):
+    COLS = ["id", "title", "created_at", 'completed_at', "user_id"]
+    TABLE_NAME = "Tasks"
+    def __init__(self, id, title, created_at, completed_at, user_id):
+            self.title = title
+            self.id = id
+            self.created_at = created_at
+            self.completed_at = completed_at
+            self.user_id = user_id
+
+    @classmethod
+    def new(cls,db, title, user_id = None): 
+        vals = [title, datetime.datetime.now(), None, user_id]
+        return insert_into_table(db, cls.TABLE_NAME, cls.COLS, vals )
+
+    @classmethod
+    def make(cls, row):
+        return Task(row[0], row[1], row[2], row[3], row[4])
+
+
+    @classmethod
+    def get_task(cls, db, task_id):
+        return get_from_table_by_id(db, cls.TABLE_NAME,  task.id, Task.make)
+
+    @classmethod
+    def complete(cls, db, task_id):
+        c = db.cursor()
+        query = """UPDATE Tasks SET completed_at=DATETIME('now') WHERE id=?"""
+        res = c.execute(query, (task_id,))
+        if res:
+            db.commit()
+            return res.lastrowid
+        else:
+            return None
+
+def insert_into_table(db, table_name, columns, values):
     c = db.cursor()
-    if user_id:
-        query = """SELECT * from Tasks WHERE user_id = ?"""
-        c.execute(query, (user_id,))
-    else:
-        query = """SELECT * from Tasks"""
-        c.execute(query)
-    tasks = []
-    rows = c.fetchall()
-    for row in rows:
-        task = make_task(row)
-        tasks.append(task)
+    query_template = """INSERT into %s values (%s)"""
+    num_cols = len(columns)
+    q_marks = ", ".join(["NULL"] + (["?"] * (num_cols-1)))
+    query = query_template%(table_name, q_marks)
+    res = c.execute(query, tuple(values))
+    if res:
+        db.commit()
+        return res.lastrowid
 
-    return tasks
+# def get_tasks(db, user_id=None):
+#     """Get all the tasks matching the user_id, getting all the tasks in the system if the user_id is not provided. Returns the results as a list of dictionaries."""
+#     c = db.cursor()
+#     if user_id:
+#         query = """SELECT * from Tasks WHERE user_id = ?"""
+#         c.execute(query, (user_id, ))
+#     else:
+#         query = """SELECT * from Tasks"""
+#         c.execute(query)
+#     tasks = []
+#     rows = c.fetchall()
+#     for row in rows:
+#         task = Task.make(row)
+#         tasks.append(task)
 
-def get_task(db, task_id):
-    """Gets a single task, given its id. Returns a dictionary of the task data."""
-    c = db.cursor()
-    query = """SELECT * from Tasks WHERE id = ?"""
-    c.execute(query, (task_id,))
-    task_row = c.fetchone()
-    if task_row:
-        return make_task(task_row)
+#     return tasks
 
-    return None
 
-def make_task(row):
-    columns = ["id", "title", "created_at", "completed_at", "user_id"]
-    return dict(zip(columns, row))
+#get functions
+
+# def get_from_table_by_id(db, table_name, id, make_dict_fn):
+#     c = db.cursor()
+#     query_template = """SELECT * from %s WHERE id = ?"""
+#     query = query_template%table_name
+#     c.execute(query, (id,))
+#     row = c.fetchone()
+#     if row:
+#         return make_dict_fn(row)
+#     return None
+
+# def get_user(db, user_id):
+#     return get_from_table_by_id(db, "Users", user_id, make_user)
+
+# def get_task(db, task_id):
+#     return get_from_table_by_id(db, "Tasks", task_id, make_task)
+
+#new functions
+# def new_user(db, email, password, name):
+#     vals = [email, password, name]            
+#     USER_COLS = ["id", "email", "password", "username"]
+#     return insert_into_table(db, "Users", USER_COLS, vals)
+    
+# def new_task(db, title, user_id = None):
+#     vals = [title, datetime.datetime.now(), None, user_id]
+#     TASK_COLS = ["id", "title", "created_at", 'completed_at', "user_id"]
+#     return insert_into_table(db, "Tasks", TASK_COLS, vals )
+
+
+    # fields = ["id", "email", "password", "username"]
+    # return dict(zip(fields, row))
+
+# def authenticate(db, email, password):
+#     c = db.cursor()
+#     query = """SELECT * from Users WHERE email=? AND password=?"""
+#     c.execute(query, (email, password))
+#     result = c.fetchone()
+#     if result:
+#         fields = ["id", "email", "password", "username"]
+#         return make_user(result)
+#     return None
+
+
+
+# def complete_task(db, task_id):
+#     """Mark the task with the given task_id as being complete."""
+#     c = db.cursor()
+#     query = """UPDATE Tasks SET completed_at=DATETIME('now') WHERE id=?"""
+#     res = c.execute(query, (task_id,))
+#     if res:
+#         db.commit()
+#         return res.lastrowid
+#     else:
+#         return None
+
+# def get_tasks(db, user_id=None):
+#     """Get all the tasks matching the user_id, getting all the tasks in the system if the user_id is not provided. Returns the results as a list of dictionaries."""
+#     c = db.cursor()
+#     if user_id:
+#         query = """SELECT * from Tasks WHERE user_id = ?"""
+#         c.execute(query, (user_id, ))
+#     else:
+#         query = """SELECT * from Tasks"""
+#         c.execute(query)
+#     tasks = []
+#     rows = c.fetchall()
+#     for row in rows:
+#         task = Task.make(row)
+#         tasks.append(task)
+
+#     return tasks
+
+
+# def make_task(row):
+#     columns = ["id", "title", "created_at", "completed_at", "user_id"]
+#     return dict(zip(columns, row))
--- tipsy.py
+++ tipsy.py
@@ -1,39 +1,74 @@
 """
 tipsy.py -- A flask-based todo list
 """
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session, g
 import model
 
 app = Flask(__name__)
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY='secret'
+)
+
+@app.before_request
+def set_up_db():
+    g.db = model.connect_db()
+
+@app.after_request
+def disconnect_db(response):
+    g.db.close()
+    return response
+
 
 @app.route("/")
 def index():
     return render_template("index.html", user_name="chriszf")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/authenticate", methods=["POST"])
+def authenticate():
+    email = request.form['email']
+    password = request.form['password']
+    user_id = model.User.authenticate(g.db, email, password)
+    session['user_id'] = user_id
+    if session['user_id'] != None:
+        return redirect("/tasks")
+    return "wrong password or email"
+
 @app.route("/save_task", methods=["POST"])
 def save_task():
-    db = model.connect_db()
+    user_id = session.get("user_id", None)
     title = request.form['title']
-    model.new_task(db, title)
+    model.Task.new(g.db, title, user_id.id)
     return redirect("/tasks")
 
 @app.route("/tasks")
 def list_tasks():
-    db = model.connect_db()
-    tasks_from_db = model.get_tasks(db, None)
+    user_id = session.get("user_id", None)
+    tasks_from_db = model.User.get_tasks(g.db, user_id.id)
     return render_template("list_tasks.html", tasks=tasks_from_db)
 
 @app.route("/task/<int:id>", methods=["GET"])
 def view_task(id):
-    db = model.connect_db()
-    task_from_db = model.get_task(db, id)
+    task_from_db = model.get_task(g.db, id)
     return render_template("view_task.html", task=task_from_db)
 
 @app.route("/task/<int:id>", methods=["POST"])
 def complete_task(id):
-    db = model.connect_db()
-    model.complete_task(db, id)
+    model.Task.complete(g.db, id)
     return redirect("/tasks")
+
+@app.route("/set_date")
+def set_date():
+    session['date'] = datetime.datetime.now()
+    return "Date set"
+
+@app.route("/get_date")
+def get_date():
+    return str(session['date'])
 
 if __name__ == "__main__":
     app.run(debug=True)
